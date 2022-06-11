2948dfbebfda99245e968fe33cfea8b5463a09a6
Tara Evans <tarajevans@hotmail.com>
2022-06-10 21:21:20 -0400

    added player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log(10+10);
+console.log("Our robot's name is "+ playerName);
+function fight () {
+    window.alert("The fight has begun!");
+}
+//fight ();
