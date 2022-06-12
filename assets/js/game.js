d4250e6f91ae6254691d44895f17e2b9f3746762
Tara Evans <tarajevans@hotmail.com>
2022-06-12 13:21:40 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -102,7 +102,48 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
+var shop = function() {
+// ask the player what they'd like to do
+var shopOptionPrompt = window.prompt(
+  "Would you like to REFILL your health, UPGRADE you attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice."
+);
+};
+switch (shopOptionPrompt) {
+  case "refill":
+    if (playerMoney >=7) {
+    window.alert("Refilling player's health by 20 f0r 7 dollars.");
 
+    //increase health and decrease money
+    playerHealth = playerHealth + 20;
+    playerMoney = playerMoney - 7;
+  }
+  else {
+    window.alert("You don't have enough money!");
+  }
+    break;
+    case "upgrade":
+      if (playerMoney>=7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+      //increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+      break;
+      case "leave":
+        window.alert("Leaving the store");
+
+        //do nothing, so function will end
+        break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+          //call shop() again to force player to pick a valid option
+          shop();
+          break
+}
 //start the game when the page loads
 startGame ()
 //function to end the entire game
@@ -195,6 +236,17 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+
+    // if we're not at the last enemy in the array
+    if (playerHealth > 0 && i < enemyNames.length - 1) {
+      //ask if player wants to use the store before next round
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+      //if yes, take them to the store() function
+      if (storeConfirm) {
+      shop();
+    }
+    }
   }
   // if player isn't alive, stop the game
   else {
